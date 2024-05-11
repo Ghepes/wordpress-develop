2339c409143025e4a0797b7ae5085c5685b68d80
Studio Wromo Marketplace <info.wromo@gmail.com>
2024-05-11 17:06:35 +0200

    add_0i

--- wp-tests-config-sample.php
+++ wp-tests-config-sample.php
@@ -37,7 +37,7 @@ define( 'WP_DEBUG', true );
  * DO NOT use a production database or one that is shared with something else.
  */
 
-define( 'DB_NAME', 'Auto-ncmaze' );
+define( 'DB_NAME', 'exampleuser' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 define( 'DB_HOST', 'localhost' );
